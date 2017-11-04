3f880d72ad2c03a59ac9a5155a08315decadfd11
adakiss <32190265+adakiss@users.noreply.github.com>
2017-11-04 13:15:16 +0100

    most basic unit tests

---
+++ Timetale/app/src/test/java/com/dpckou/agoston/timetale/HourTest.java
@@ -0,0 +1,38 @@
+package com.dpckou.agoston.timetale;
+
+import com.dpckou.agoston.timetale.weekday.Hour;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by kissa on 2017. 11. 04..
+ */
+
+public class HourTest {
+
+    Hour hour;
+
+    @Before
+    public void setUp() {
+        hour = new Hour(0);
+    }
+
+    @Test
+    public void hourTest1() {
+        Assert.assertEquals(hour.getHour(), (Integer)0);
+        Assert.assertNull(hour.getEvent());
+        Assert.assertFalse(hour.isEventPlanned());
+    }
+
+    @Test
+    public void hourTest2() {
+        String event = "Basketball training";
+        hour.setEvent(event);
+
+        Assert.assertEquals(hour.getHour(), (Integer)0);
+        Assert.assertEquals(hour.getEvent(), event);
+        Assert.assertTrue(hour.isEventPlanned());
+    }
+}
